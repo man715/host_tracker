b0b1c5b344e73d25c050e3ca4999571145d9c507
root <root@jk.edu.state.wy.us>
2020-02-12 13:46:23 -0700

    Added the check for host already existing. Also, handled the error in an alert div.

--- routes.rb
+++ routes.rb
@@ -29,7 +29,6 @@ get '/host_tracker/:id/hosts' do
   @report = get_report(id)
   @exist = params[:exist]
   @attempted_host = params[:attempted_host] 
-  return @attempted_host
   @plugin_side_menu = get_plugin_list('user') # TODO: Look into how this works
 
   #return 'No Such Report' if @report.nil? # # TODO: Handle this a little more gracefully. Maybe an alert?
@@ -75,8 +74,8 @@ post '/host_tracker/:id/hosts/new' do
   if @exist == false 
     @host.save
   end 
-  # for a parameter_pollution on report_id
-  redirect to("/host_tracker/#{id}/hosts?exist=true?attempted_host=#{@host.ip}")
+
+  redirect to("/host_tracker/#{id}/hosts?exist=#{@exist}&attempted_host=#{@host.ip}")
 end
 
 # Delete a template host
